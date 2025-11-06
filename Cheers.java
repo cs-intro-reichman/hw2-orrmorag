0f51df2dd174bcf07605af4ff241fdc5585bd3f7
Orr <orrmorag@post.runi.ac.il>
2025-11-06 16:02:46 +0200

    Cheers program complete

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,19 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                int times = Integer.parseInt(args[1]);
+                for (int i = 0; i < word.length(); i++) {
+                        char ch = word.charAt(i);
+                        if ("AEFHILMNORSX".indexOf(word) == -1) {
+                                System.out.println("Give me a " + ch + ": " + ch + "!");
+                        }
+                        else {
+                                System.out.println("Give me an " + ch + ": " + ch + "!");
+                        }
+                }
+                System.out.println("What does that spell?");
+                for (int i = 0; i < times; i++) {
+                        System.out.println(word + "!!!");
+                }
         }
 }
