970c74c177996cd9d403e245956dcce004d4e830
Orr <orrmorag@post.runi.ac.il>
2025-11-06 19:08:12 +0200

    Cheers program complete

--- Cheers.java
+++ Cheers.java
@@ -1,11 +1,11 @@
 public class Cheers {
         public static void main(String[] args) {
-                String word = args[0];
+                String word = args[0].toUpperCase();
                 int times = Integer.parseInt(args[1]);
                 for (int i = 0; i < word.length(); i++) {
                         char ch = word.charAt(i);
                         if ("AEFHILMNORSX".indexOf(ch) == -1) {
-                                System.out.println("Give me a  " + ch + ": " + ch + "!");
+                                System.out.println("Give me a " + ch + ": " + ch + "!");
                         }
                         else {
                                 System.out.println("Give me an " + ch + ": " + ch + "!");
