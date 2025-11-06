f91a5354b9eb52138dcfa91839fa1c9b5c5a2269
Orr Morag <orr.morag@post.runi.ac.il>
2025-11-06 21:29:04 +0200

    Cheers program complete

--- Cheers.java
+++ Cheers.java
@@ -2,13 +2,29 @@ public class Cheers {
         public static void main(String[] args) {
                 String word = args[0].toUpperCase();
                 int times = Integer.parseInt(args[1]);
+                boolean flag = false;
                 for (int i = 0; i < word.length(); i++) {
                         char ch = word.charAt(i);
-                        if ("AEFHILMNORSX".indexOf(ch) == -1) {
-                                System.out.println("Give me a " + ch + ": " + ch + "!");
+                        if ("AEFHILMNORSX".indexOf(ch) != -1) {
+                                flag = true;
+                                break;
                         }
-                        else {
-                                System.out.println("Give me an " + ch + ": " + ch + "!");
+                }
+                if (flag == true) {
+                        for (int i = 0; i < word.length(); i++) {
+                                char ch = word.charAt(i);
+                                if ("AEFHILMNORSX".indexOf(ch) == -1) {
+                                        System.out.println("Give me a  " + ch + ": " + ch + "!");
+                                }
+                                else {
+                                        System.out.println("Give me an " + ch + ": " + ch + "!");
+                                }
+                        }
+                }
+                else {
+                        for (int i = 0; i < word.length(); i++) {
+                                char ch = word.charAt(i);
+                                System.out.println("Give me a " + ch + ": " + ch + "!");
                         }
                 }
                 System.out.println("What does that spell?");
@@ -16,5 +32,4 @@ public class Cheers {
                         System.out.println(word + "!!!");
                 }
         }
-
 }
